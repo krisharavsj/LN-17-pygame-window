ad902e83961841b353785203c9492bc84b089b77
krisharavsj <krisharsv@gmail.com>
2025-08-18 20:57:54 +0530

    colorfulbounce.py

--- add image.py
+++ add image.py
@@ -1,25 +1,29 @@
 import pygame
 pygame.init()
-SCREEN_HEIGHT,SCREEN_WIDTH=(500,500)
-display_surface=pygame.display.set_mode((SCREEN_HEIGHT,SCREEN_WIDTH))
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
+display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Adding image and background image')
-background_image=pygame.transform.scale(pygame.image.load('background.jpg').convert(),(SCREEN_HEIGHT,SCREEN_WIDTH))
-penguin_image=pygame.transform.scale(pygame.image.load('penguin.png').convert_alpha(),(200,200))
-penguin_rect=penguin_image.get_rect(center=(SCREEN_WIDTH//2,SCREEN_HEIGHT//2-30))
-text=pygame.font.Font(None,36).render('hello world',True,pygame.Color('black'))
-text_rect=text.get_rect(center=(SCREEN_WIDTH//2,SCREEN_HEIGHT//2+110))
+background_image = pygame.transform.scale(
+    pygame.image.load('background.jpg').convert(),
+    (SCREEN_WIDTH, SCREEN_HEIGHT))
+penguin_image = pygame.transform.scale(
+    pygame.image.load('penguin.png').convert_alpha(),
+    (200, 200))
+penguin_rect = penguin_image.get_rect(center=(SCREEN_WIDTH//2, SCREEN_HEIGHT//2 - 30))
+text = pygame.font.Font(None, 36).render('hello world', True, pygame.Color('black'))
+text_rect = text.get_rect(center=(SCREEN_WIDTH//2, SCREEN_HEIGHT//2 + 110))
 def game_loop():
-    clock=pygame.time.Clock()
-    running=True
+    clock = pygame.time.Clock()
+    running = True
     while running:
         for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                running=False
-        display_surface.blit(background_image,(0,0))
-        display_surface.blit(penguin_image,penguin_rect)
-        display_surface.blit(text,text_rect)
+            if event.type == pygame.QUIT:
+                running = False
+        display_surface.blit(background_image, (0, 0))
+        display_surface.blit(penguin_image, penguin_rect)
+        display_surface.blit(text, text_rect)
         pygame.display.flip()
-        clock.tick(30)
+        clock.tick(5)
     pygame.quit()
-if __name__=='__main__':
-    game_loop
+if __name__ == '__main__':
+    game_loop()
