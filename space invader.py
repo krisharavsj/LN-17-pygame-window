024d934862e6e2727805ca2b284af2a5efcf708a
krisharavsj <krisharsv@gmail.com>
2025-08-29 20:57:01 +0530

    space invader.py

--- space invader.py
+++ space invader.py
@@ -33,3 +33,53 @@ enemyY=[]
 enemyX_change=[]
 enemyY_change=[]
 number_of_enemies=6
+for i in range(number_of_enemies):
+    enemy_image.append(pygame.image.load('alien_image-removebg-preview.png'))
+    enemyX.append(random.randint(0,SCREEN_WIDTH-64))
+    enemyY.append(random.randint(ENEMY_START_Y_MIN,ENEMY_START_Y_MAX))
+    enemyX_change.append(ENEMY_SPEED_X)
+    enemyY_change.append(ENEMY_SPEED_Y)
+bullet_image=pygame.image.load('bullet-removebg-preview.png')
+bulletX=0
+bulletY=PLAYER_START_Y
+bulletX_change=0
+bulletY_change=BULLET_SPEED_Y
+bullet_state="ready"
+
+score_value=0
+font=pygame.font.Font('timesnewroman',64)
+def show_score(x,y):
+    score=font.render("Score:"+str(score_value),True,(255,255,255))
+    screen.blit(score,(x,y))
+def game_over_text():
+    over_text=font.render("GAMEOVER",True,(255,255,255))
+    screen.blit(over_text,(200,250))
+def player(x,y):
+    screen.blit(player_image,(x,y))
+def enemy(x,y,i):
+    screen.blit(enemy_image[i],(x,y))
+def bullet(x,y):
+    global bullet_state
+    bullet_state="fire"
+    screen.blit(bullet_image,(x+16,y+10))
+def isCollision(enemyX,enemyY,bulletX,bulletY):
+    distance=math.sqrt((enemyX-bulletX)**2+(enemyY-bulletY)**2)
+    return distance<COLLISION_DISTANCE
+running=True
+while running:
+    screen.fill((0,0,0))
+    screen.blit(bg_image,(0,0))
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_LEFT:
+                playerX_change=-5
+            if event.key==pygame.K_RIGHT:
+                playerX_change=+5
+            if event.key==pygame.K_SPACE and bullet_state=="ready":
+                bulletX=playerX
+                bullet(bulletX,bulletY)
+        if event.type==pygame.KEYUP and event.key in [pygame.K_LEFT,pygame.K_RIGHT]:
+            playerX_change=0
+        
